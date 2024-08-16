438803ef14896f58dabd34e4ae2b4fdf3c0114e5
Shashank Shreshtha <109666224+ShashankShreshtha@users.noreply.github.com>
2024-08-16 17:10:12 +0530

    Update Card War Game.py

--- Card War Game.py
+++ Card War Game.py
@@ -1,23 +1,4 @@
-# how to play "War" here are the basic rules:
-
-# The deck is divided evenly, with each player receiving 26 cards, dealt one at a time,
-# face down. Anyone may deal first. Each player places his stack of cards face down,
-# in front of him.
-
-# The Play:
-# Each player turns up a card at the same time and the player with the higher card
-# takes both cards and puts them, face down, on the bottom of his stack.
-
-# If the cards are the same rank, it is War. Each player turns up three cards face
-# down and one card face up. The player with the higher cards takes both piles
-# (six cards). If the turned-up cards are again the same rank, each player places
-# another card face down and turns another card face up. The player with the
-# higher card takes all 10 cards, and so on.
-
-# There are some more variations on this but we will keep it simple for now.
-# Ignore "double" wars
 from random import shuffle
-
 # Two useful variables for creating Cards.
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
